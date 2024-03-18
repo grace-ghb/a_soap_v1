e783e3877fb3dd07951ce2de3148b77efeb9dca5
Grace How <gracehow8702@gmail.com>
2024-03-18 22:49:46 +0000

    Added product details

--- a_soap/settings.py
+++ a_soap/settings.py
@@ -70,8 +70,10 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
