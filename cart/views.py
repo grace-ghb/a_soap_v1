b08e1171a21294f980e3734333aa1d5d53a3efe4
Grace How <gracehow8702@gmail.com>
2024-03-27 23:07:09 +0000

    Added toasts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
+
 
 def view_cart(request):
     """
@@ -9,6 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -17,6 +22,7 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
