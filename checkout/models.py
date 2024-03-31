38383dec86dd98025f07c928fcadbf776b124bf7
Grace How <gracehow8702@gmail.com>
2024-03-31 22:03:34 +0000

    Install django-countries[pyuca] and add 'django_countries' in INSTALLED_APPS

--- checkout/models.py
+++ checkout/models.py
@@ -10,13 +10,13 @@ from products.models import Product
 
 class Order(models.Model):
     """
-    Track orders for user purchase
+    To create and track orders for user purchase
     """
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label="(select country)", null=False, blank=False)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=100, null=False, blank=False)
@@ -64,9 +64,12 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
+    """
+    The model for specific product line item
+    """
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    # product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
