306756b4aee836eb4200724302ec818feb0c5076
Grace How <gracehow8702@gmail.com>
2024-04-08 01:00:03 +0000

    Create subscription app

--- a_soap/urls.py
+++ a_soap/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
-    path('', include('blog.urls')),
+    path('blog', include('blog.urls')),
+    path('subscription', include('subscription.urls')),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ subscription/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import SubscribeForm
+from .models import Subscription
+from django.contrib import messages
+from django.core.mail import send_mail
+
+
+def subscribe(request):
+    """
+    Subscription functionality for newsletter
+    """
+    if request.method == "POST":
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, "You have subscribed to our newsletter! Thank you.")
+            email = request.POST.get("email")
+            subject = "Homemade Soap"
+            message = (
+                "Thank you for subscribing to our newsletter,"
+                + " you will get latest updates on our new products and deals in upcoming email!"
+                + " or you can visit our website at"
+                + " https://#.herokuapp.com/"
+
+            )
+            from_email = "admin@asoap.com"
+            recipient_list = [email]
+            send_mail(subject,
+                      message, from_email, recipient_list, fail_silently=False)
+            return redirect("/")
+    else:
+        form = SubcribeForm()
+
+    context = {"form": form}
+    return render(request, "index.html", context)
