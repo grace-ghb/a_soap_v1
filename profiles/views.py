6b476d9c3346086efa1de83d21875620b885962b
Grace How <gracehow8702@gmail.com>
2024-04-11 09:00:02 +0000

    Added add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
-@login_required
 def profile(request):
-    """ 
-    Display the user's profile. 
-    """
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -19,8 +15,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -32,7 +30,7 @@ def profile(request):
 
     return render(request, template, context)
 
-    
+
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
